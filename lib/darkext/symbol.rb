482106d7b9cad6faaa42660d6ae246e25adf76e5
Daniel Huckstep <darkhelmet@darkhelmetlive.com>
2008-11-25 14:21:47 -0700

    use activerecord's code for to_proc on symbol, and gaurd against doing it multiple times

--- lib/darkext/symbol.rb
+++ lib/darkext/symbol.rb
@@ -1,5 +1,9 @@
-class Symbol
-  def to_proc
-    proc { |obj,*args| obj.send(self, *args) }
+unless :to_proc.respond_to?(:to_proc)
+  class Symbol
+    # Does exactly what the activesupport version does,
+    # with pretty much the same code.
+    def to_proc
+      Proc.new { |*args| args.shift.__send__(self, *args) }
+    end
   end
 end
