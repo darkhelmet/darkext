4511d1bc7e3eb6c29ca1da0bffa9cf48a4a56401
Daniel Huckstep <darkhelmet@darkhelmetlive.com>
2008-11-26 02:24:06 -0700

    remove respond_to stuff

--- lib/darkext/symbol.rb
+++ lib/darkext/symbol.rb
@@ -1,9 +1,7 @@
 class Symbol
   # Does exactly what the activesupport version does,
   # with pretty much the same code.
-  unless :to_proc.respond_to?(:to_proc)
-    def to_proc
-      Proc.new { |*args| args.shift.__send__(self, *args) }
-    end
+  def to_proc
+    Proc.new { |*args| args.shift.__send__(self, *args) }
   end
 end
