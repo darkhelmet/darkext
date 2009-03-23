f43cc1ea3648dd8156101e73c8b5081577b5f86e
Daniel Huckstep <darkhelmet@darkhelmetlive.com>
2009-03-23 15:11:50 -0600

    simpler autoload

--- lib/darkext.rb
+++ lib/darkext.rb
@@ -6,8 +6,8 @@ libs = Dir[File.join(File.join(File.dirname(__FILE__), 'darkext'), '*.rb')]
 # Don't load these by default
 other_libs = libs.reject! { |lib| lib.include?('sinatra') || lib.include?('sitemap') }
 
-beagle = libs.reject! { |lib| lib.include?('beagle') }.first
+libs.reject! { |lib| lib.include?('beagle') }.first
 libs.each { |lib| require lib }
 
 # autoload beagle
-autoload(:Beagle, File.join(File.dirname(__FILE__),'darkext/beagle.rb'))
+autoload(:Beagle, 'darkext/beagle')
