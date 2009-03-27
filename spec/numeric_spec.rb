a29ad3dc30db5468cfd376a16cc52d8ef04ad976
Daniel Huckstep <darkhelmet@darkhelmetlive.com>
2009-03-27 12:42:42 -0600

    numeric specs

--- spec/numeric_spec.rb
+++ spec/numeric_spec.rb
@@ -1,1 +1,19 @@
 require File.dirname(__FILE__) + '/spec_helper'
+
+describe Numeric do
+  it 'should respond to the new methods' do
+    %w(square cube sqrt root ln log).each do |method|
+      [10,10.5].each do |num|
+        num.respond_to?(method).should == true
+      end
+    end
+  end
+
+  it 'should return Numeric from most new methods' do
+    %w(square cube sqrt root ln log).each do |method|
+      [10,10.5].each do |num|
+        num.send(method.intern).is_a?(Numeric).should == true
+      end
+    end
+  end
+end
